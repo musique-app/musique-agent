dae97194516464150c80c6157d9168f0e3ab2fc5
polemonLX <diogo.ventura6@hotmail.com>
2025-03-30 20:19:32 +0100

    Updated logs and Dockerfile

--- src/main/java/com/projeto_musique/core/player/MP3.java
+++ src/main/java/com/projeto_musique/core/player/MP3.java
@@ -3,6 +3,7 @@ package com.projeto_musique.core.player;
 import com.projeto_musique.core.SoundPlayer;
 import com.projeto_musique.models.SoundData;
 import javazoom.jl.player.advanced.AdvancedPlayer;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.InputStream;
 import java.net.URL;
@@ -10,6 +11,7 @@ import java.net.URL;
 /**
  * Implementation of a sound using MP3 codec.
  */
+@Slf4j
 public class MP3 implements SoundPlayer {
 
     /**
@@ -22,7 +24,7 @@ public class MP3 implements SoundPlayer {
             AdvancedPlayer player = new AdvancedPlayer(audioStream);
             player.play();
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error(e.getMessage(), e);
         }
     }
 
